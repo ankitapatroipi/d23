ed2220a7adc3b9a8fc6161cc0c9aeb80c0dae036
ankitapatroipi <ankitapatroipi@gmail.com>
2018-11-30 20:43:37 -0500

    now done

--- event2.js
+++ event2.js
@@ -4,7 +4,7 @@ var eventEmitter = new events.EventEmitter();
 
 var f1 = function () 
 {     
-	console.log("Test Event is happens.");
+	console.log("Test Event is happening");
 };
 
 eventEmitter.on("TestEvent", f1);
